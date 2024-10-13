fc34861982bf2d286725ccc015747dd6799edfa9
abhijeetkumarshukla <shuklaabhijeet510@gmail.com>
2024-10-13 16:45:36 +0530

    countdown done

---
+++ CountdownTimer/src/App.jsx
@@ -0,0 +1,16 @@
+ import './App.css'
+import CountdownTimer from './CountdownTimer'
+
+function App() {
+ 
+
+  return (
+    <>
+       <div>
+      <CountdownTimer initialTime={120} />  
+    </div>
+    </>
+  )
+}
+
+export default App
